25f14d85f51f627bfffe700ea6ae0301a00bb534
Ray Chen <oldsharp@gmail.com>
2016-08-23 23:49:35 +0800

    Multi-machine support for ubuntu/trusty64

--- ubuntu/trusty64/bootstrap.sh
+++ ubuntu/trusty64/bootstrap.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-# Setup system-wide mirror for package-managing:
+# Get hostname and IP from command-line args.
+hostname=$1
+ipaddr=$2
+
+# Setup system-wide mirror for package-managing.
 cat > /etc/apt/sources.list << EOF
 deb http://mirrors.ustc.edu.cn/ubuntu trusty main restricted universe multiverse
 deb http://mirrors.ustc.edu.cn/ubuntu trusty-security main restricted universe multiverse
@@ -14,3 +18,18 @@ deb-src http://mirrors.ustc.edu.cn/ubuntu trusty-proposed main restricted univer
 deb-src http://mirrors.ustc.edu.cn/ubuntu trusty-backports main restricted universe multiverse
 EOF
 apt-get update
+
+echo 'nameserver 119.29.29.29' > /etc/resolvconf/resolv.conf.d/base
+
+timedatectl set-timezone 'Asia/Shanghai'
+apt-get -y install ntpdate
+ntpdate pool.ntp.org
+apt-get -y install ntp
+service ntp start
+
+hostnamectl set-hostname "$hostname.localdomain"
+echo "$ipaddr	$hostname.localdomain	$hostname" >> /etc/hosts
+
+adduser --disabled-password --gecos '' example
+echo 'example:example' | chpasswd
+echo 'example ALL=(ALL) NOPASSWD:ALL' > /etc/sudoers.d/example
