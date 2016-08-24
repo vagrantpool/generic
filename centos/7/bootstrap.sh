305b29f7d76b60007aaa35ce2eb8437086109007
Ray Chen <oldsharp@gmail.com>
2016-08-24 13:43:21 +0800

    Add example files for centos/7

---
+++ centos/7/bootstrap.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+# Get hostname and IP from command-line args.
+hostname=$1
+ipaddr=$2
+
+# Setting system-wide mirror for package-managing.
+tee /etc/yum.repos.d/CentOS-Base.repo <<-'EOF'
+[base]
+name=CentOS-$releasever - Base
+baseurl=http://mirrors.ustc.edu.cn/centos/$releasever/os/$basearch/
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+
+[updates]
+name=CentOS-$releasever - Updates
+baseurl=http://mirrors.ustc.edu.cn/centos/$releasever/updates/$basearch/
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+
+[extras]
+name=CentOS-$releasever - Extras
+baseurl=http://mirrors.ustc.edu.cn/centos/$releasever/extras/$basearch/
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+
+[centosplus]
+name=CentOS-$releasever - Plus
+baseurl=http://mirrors.ustc.edu.cn/centos/$releasever/centosplus/$basearch/
+gpgcheck=1
+enabled=0
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+EOF
+
+yum -y makecache
+
+timedatectl set-timezone 'Asia/Shanghai'
+
+hostnamectl set-hostname "$hostname.localdomain"
+echo "$ipaddr	$hostname.localdomain	$hostname" >> /etc/hosts
+
+adduser benchmark
+echo 'benchmark:benchmark' | chpasswd
+usermod -aG wheel benchmark
+echo 'benchmark ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/benchmark
