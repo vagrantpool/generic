0fde39ac5a0d8f2e61cd5daae99da2831c52d92f
Ray Chen <oldsharp@gmail.com>
2016-11-08 21:52:47 +0800

    Add centos/6 support

---
+++ centos/6/bootstrap.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+# Get hostname and IP from command-line args.
+hostname=$1
+ipaddr=$2
+
+NEWUSER=example
+PASSWD=example
+
+# Setting system-wide mirror for package-managing.
+tee /etc/yum.repos.d/CentOS-Base.repo <<-'EOF'
+[base]
+name=CentOS-$releasever - Base
+baseurl=http://mirrors.ustc.edu.cn/centos/$releasever/os/$basearch/
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6
+
+[updates]
+name=CentOS-$releasever - Updates
+baseurl=http://mirrors.ustc.edu.cn/centos/$releasever/updates/$basearch/
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6
+
+[extras]
+name=CentOS-$releasever - Extras
+baseurl=http://mirrors.ustc.edu.cn/centos/$releasever/extras/$basearch/
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6
+
+[centosplus]
+name=CentOS-$releasever - Plus
+baseurl=http://mirrors.ustc.edu.cn/centos/$releasever/centosplus/$basearch/
+gpgcheck=1
+enabled=0
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6
+EOF
+
+yum -y makecache
+
+timedatectl set-timezone 'Asia/Shanghai'
+
+hostnamectl set-hostname "$hostname.localdomain"
+echo "$ipaddr	$hostname.localdomain	$hostname" >> /etc/hosts
+
+adduser $NEWUSER
+echo "$NEWUSER:$PASSWD" | chpasswd
+usermod -aG wheel $NEWUSER
+echo "$NEWUSER ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/$NEWUSER
